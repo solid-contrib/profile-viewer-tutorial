1d222fcd9a9923741dcf17cccb28b50933eb6966
Ruben Verborgh <ruben@verborgh.org>
2018-08-01 14:01:29 -0700

    Add login functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
-$(() => {
-  displayLoginStatus();
+$(async () => {
+  $('#login button').click(login);
+  await displayLoginStatus();
 });
 
-function displayLoginStatus() {
-  $('#logout').hide();
+async function login() {
+  await SolidAuthClient.popupLogin({ popupUri: 'scripts/login.html' });
+  displayLoginStatus();
+}
+
+async function displayLoginStatus() {
+  const session = await SolidAuthClient.currentSession();
+  // The user is not logged in
+  if (!session) {
+    $('#login').show();
+    $('#logout').hide();
+  }
+  // The user is logged in
+  else {
+    $('#login').hide();
+    $('#logout').show();
+    $('#user').text(session.webId);
+  }
 }
