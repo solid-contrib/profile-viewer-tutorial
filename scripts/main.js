5c0dfa1afba75e5b9bcc77d1fbc43e662363844a
Ruben Verborgh <ruben@verborgh.org>
2018-08-01 14:01:29 -0700

    Add login functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-$('#logout').hide();
+// Log the user in on click
+const popupUri = 'popup.html';
+$('#login button').click(() => solid.auth.popupLogin({ popupUri }));
+
+// Update components to match the user's login status
+solid.auth.trackSession(session => {
+  const loggedIn = !!session;
+  $('#login').toggle(!loggedIn);
+  $('#logout').toggle(loggedIn);
+  $('#user').text(session && session.webId);
+});
