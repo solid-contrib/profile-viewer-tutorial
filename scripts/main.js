24ef8a4725107fc6b1d1df258e68c39e59fdc155
Ruben Verborgh <ruben@verborgh.org>
2018-08-01 14:03:16 -0700

    Add logout functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-// Log the user in on click
+// Log the user in and out on click
 const popupUri = 'popup.html';
-$('#login button').click(() => solid.auth.popupLogin({ popupUri }));
+$('#login  button').click(() => solid.auth.popupLogin({ popupUri }));
+$('#logout button').click(() => solid.auth.logout());
 
 // Update components to match the user's login status
 solid.auth.trackSession(session => {
